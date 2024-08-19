b7d08a9abc5c4447827a6b35fe3be841a0b47405
victoria parchevskaya <parchevskayavictoria@gmail.com>
2024-08-19 16:05:13 +0300

    feat: add configuration and logic to main

--- src/main/java/edu/vsp/Main.java
+++ src/main/java/edu/vsp/Main.java
@@ -1,8 +1,24 @@
 package edu.vsp;
 
+import edu.vsp.configuration.CsvConfiguration;
+import edu.vsp.repository.UserRepository;
+import edu.vsp.repository.csv.UserCsvRepository;
+import edu.vsp.service.ConsoleInputReader;
+import edu.vsp.service.ConsoleService;
+import edu.vsp.service.UserValidatorService;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CsvConfiguration csvConfiguration = new CsvConfiguration();
+        UserValidatorService userValidatorService = new UserValidatorService();
+        ConsoleInputReader consoleInputReader = new ConsoleInputReader(scanner);
+        UserRepository userRepository = new UserCsvRepository(csvConfiguration);
+        ConsoleService consoleService = new ConsoleService(consoleInputReader, userRepository, userValidatorService);
 
+        consoleService.giveUserMainChoice();
     }
 }
